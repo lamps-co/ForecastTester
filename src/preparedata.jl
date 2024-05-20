f2e6395c259dc50b8dedc9a774e466523aae8fe0
dietzemarina <marinadietze@lampsco.com>
2024-05-20 18:22:30 -0300

    fixed error eachindex

--- src/preparedata.jl
+++ src/preparedata.jl
@@ -11,7 +11,7 @@
 function build_train_test_dict(df_train::DataFrame, df_test::DataFrame)::Dict{Int, Dict{String, Vector{Float64}}}
     train_test_dict = Dict()
 
-    for i in 1:eachindex(df_train[:, 1])
+    for i in eachindex(df_train[:, 1])
         y_raw = Vector(df_train[i, :])[2:end]
         y_train_raw = y_raw[1:findlast(i->!ismissing(i), y_raw)]
         T = length(y_train_raw)
